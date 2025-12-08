b0dc6dfab191d106b42f98f812f18387ce659346
galaroha1 <galaroha@sas.upenn.edu>
2025-12-07 19:46:41 -0500

    Fix scrolling behavior in LogisticsHub

--- src/pages/LogisticsHub.tsx
+++ src/pages/LogisticsHub.tsx
@@ -133,7 +133,7 @@ export function LogisticsHub() {
     };
 
     return (
-        <div className="flex h-[calc(100vh-4rem)] flex-col gap-6">
+        <div className="flex flex-1 flex-col gap-6 h-full overflow-hidden">
             {/* Header */}
             <div className="flex flex-col gap-4 border-b border-slate-200 pb-6 sm:flex-row sm:items-center sm:justify-between">
                 <div>
